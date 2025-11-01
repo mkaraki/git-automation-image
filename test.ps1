367b9ea4f421841155fa8e10ddb8d79de3f9e671
mkaraki <26180919+mkaraki@users.noreply.github.com>
2025-11-02 01:48:21 +0900

    Fix test script

--- test.ps1
+++ test.ps1
@@ -3,13 +3,18 @@
 Import-Module Sentry
 
 Start-Sentry {
+    $_.Dsn = $env:SENTRY_DSN
     $_.SendDefaultPii = $false
+}
+
+trap {
+    $_ | Out-Sentry
 }
 
 $transaction = Start-SentryTransaction 'task-test.ps1' 'test.ps1'
 
 $span = $transaction.StartChild("file.mkdir")
-New-Item -Force /tmp/image-test
+New-Item -Force -ItemType Directory /tmp/image-test
 $span.Finish()
 
 Set-Location /tmp/image-test
